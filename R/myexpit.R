a636540cedb6c0c337c30a76c1a5bb6e8daf3e70
Aya Mitani <ayamitani@gmail.com>
2021-01-19 15:49:08 -0500

    initial commit

---
+++ R/myexpit.R
@@ -0,0 +1,13 @@
+#' @title inverse logistic link function
+#' @description a function that returns the inverse of the logistic link function
+#' @param x numeric vector
+#' @return y
+#' @author Aya Mitani
+#' @examples
+#' myexpit(2)
+#' @export
+
+myexpit <- function(x){
+  y <- exp(x) / (1 + exp(x))
+  return(y)
+}
